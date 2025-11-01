8c7a1b831d0aa401966069f02bf48c36bdbda317
JPBA <jpb@a.com>
2025-11-01 08:53:05 +0000

    Main returns wordcount

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 def get_book_text(filepath):
     with open(f"./{filepath}") as f:
         file_contents = f.read()
-        print(file_contents)
+        words = file_contents.split()
+        word_count = len(words)
+        print(f"Found {word_count} total words")
     return
 
 get_book_text("books/frankenstein.txt")
