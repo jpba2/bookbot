8c5d5ce1c172ca307f078129a0bc9a90583fcb12
JPBA <jpb@a.com>
2025-11-02 21:32:01 +0000

    updating def book_report

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-from stats import get_num_words, get_character_count, sorted_characters, sort_on
-
+from stats import (get_num_words,
+                   get_character_count,
+                   sorted_characters,
+                   sort_on
+)
 
 file_path = "books/frankenstein.txt"
-word_count = get_num_words(file_path)
-character_count = sorted_characters(get_character_count(file_path))
 
-print("============ BOOKBOT ============")
-print(f"Analyzing book found at {file_path}...")
-print("----------- Word Count ----------")
-print(f"Found {word_count} total words")
-print("--------- Character Count -------")
-for i in range(0, len(character_count)):
-    print(f"{character_count[i]["char"]}: {character_count[i]["num"]}")
-print("============= END ===============")
+def book_report(file_path):
+    word_count = get_num_words(file_path)
+    character_count = sorted_characters(get_character_count(file_path))
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for i in range(0, len(character_count)):
+        print(f"{character_count[i]["char"]}: {character_count[i]["num"]}")
+    print("============= END ===============")
+    
 
-
+book_report(file_path)
