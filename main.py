6be6d75e4e4c4edcdb02d28d217dc18ae0997255
JPBA <jpb@a.com>
2025-11-02 14:53:08 +0000

    Stats.py added

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-def get_book_text(filepath):
-    with open(f"./{filepath}") as f:
-        file_contents = f.read()
-        words = file_contents.split()
-        word_count = len(words)
-        print(f"Found {word_count} total words")
-    return
-
-get_book_text("books/frankenstein.txt")
+from stats import get_num_words
---
+++ stats.py
@@ -0,0 +1,9 @@
+def get_num_words(filepath):
+    with open(f"./{filepath}") as f:
+        file_contents = f.read()
+        words = file_contents.split()
+        word_count = len(words)
+        print(f"Found {word_count} total words")
+    return 
+
+get_num_words("/books/frankenstein.txt")
